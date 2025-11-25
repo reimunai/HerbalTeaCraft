e9743b507ac598b94acd9f0a7ac7d00e5f3b51fb
reinai <qwq13172558756@163.com>
2025-11-25 23:22:12 +0800

    pot & windbox

--- Assets/Scripts/PotManager.cs
+++ Assets/Scripts/PotManager.cs
@@ -7,7 +7,9 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class PotManager : MonoBehaviour
 {
     public BrewingPot pot;
-
+    public float potTemperatureDecreseSpeed = 5f;
+    public WindBoxGrabInteractor windBox;
+    
     public XRSocketInteractor XRSocketInteractor;
     public Ingredient ingredient1;
     // Start is called before the first frame update
@@ -20,5 +22,16 @@ public class PotManager : MonoBehaviour
     private void Update()
     {
         pot.UpdateQualityColor(Time.deltaTime);
+        if (pot.isHeating)
+        {
+            pot.AdjustTemperature(pot.temperature - potTemperatureDecreseSpeed * Time.deltaTime + windBox.pullSpeed * 0.1f);
+        }
+    }
+
+    public void OnStartHeatingBtn(SelectEnterEventArgs args)
+    {
+        Debug.Log("OnStartHeatingBtn");
+        pot.StartHeating();
+        pot.temperature = 0;
     }
 }
